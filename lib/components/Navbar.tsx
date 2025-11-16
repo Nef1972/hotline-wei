584d52e126f3af9a240084aff7caa88d4038cc5a
Malo Bonnefoy <malo.bonnefoy@student-cs.fr>
2025-11-16 18:56:24 +0100

    feat: Navbar message

--- lib/components/Navbar.tsx
+++ lib/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { UserButton } from "@clerk/nextjs";
 export function Navbar() {
   return (
     <nav className="flex items-center justify-between p-4 bg-stone-800 text-white">
-      <h1 className="text-xl font-bold">BDE Gestion App</h1>
+      <h1 className="text-xl font-bold">Hotline Wei</h1>
       <UserButton />
     </nav>
   );
