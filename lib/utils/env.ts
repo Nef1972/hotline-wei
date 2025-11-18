f2f28551a24f2ba2cb5ad6c26de756f48a2612d0
Paulin <paulin.bonnefoy@outlook.fr>
2025-11-18 01:04:02 +0100

    feat: fetch user info by jwt

--- lib/utils/env.ts
+++ lib/utils/env.ts
@@ -1,6 +1,7 @@
 const env = {
   appUrl: process.env.NEXT_PUBLIC_APP_URL!,
   databaseUrl: process.env.DATABASE_URL!,
+  tokenTemplate: process.env.CLERK_JWT_TEMPLATE_NAME!,
 } as const;
 
 export default env;
