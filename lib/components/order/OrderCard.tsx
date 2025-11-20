434cb32bbeba5cd12b00f0e3f726e31af626355e
Paulin <paulin.bonnefoy@outlook.fr>
2025-11-20 01:58:14 +0100

    feat: popconfirm order deletion

--- lib/components/order/OrderCard.tsx
+++ lib/components/order/OrderCard.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { Tooltip } from "antd";
-import { Order } from "@/lib/types/Order";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { DoneTag } from "@/lib/components/tag/DoneTag";
-import { InProgressTag } from "@/lib/components/tag/InProgressTag";
-import { useMutation } from "@tanstack/react-query";
+import {Popconfirm, Tooltip} from "antd";
+import {Order} from "@/lib/types/Order";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
+import {DoneTag} from "@/lib/components/tag/DoneTag";
+import {InProgressTag} from "@/lib/components/tag/InProgressTag";
+import {useMutation} from "@tanstack/react-query";
 import axios from "axios";
-import { queryClient } from "@/lib/query/queryClient";
+import {queryClient} from "@/lib/query/queryClient";
 import useNotification from "@/lib/hooks/useNotification";
 
 type OrderCardProps = {
@@ -52,13 +52,21 @@ export const OrderCard = ({ order }: OrderCardProps) => {
   return (
     <Tooltip title={order.description}>
       <div className="bg-white dark:bg-zinc-950 rounded-2xl shadow-md p-6 flex flex-col justify-between relative">
-        <button
-          onClick={() => mutate()}
-          className="absolute top-3 right-3 text-red-500 hover:text-red-700"
-          title="Supprimer"
+        <Popconfirm
+          title="Supprimer cette commande ?"
+          description="Cette action est irréversible."
+          okText="Oui"
+          cancelText="Non"
+          placement="topRight"
+          onConfirm={() => mutate()}
         >
-          <FontAwesomeIcon icon={faTrash} />
-        </button>
+          <button
+            className="absolute top-3 right-3 text-red-500 hover:text-red-700"
+            title="Supprimer"
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </Popconfirm>
 
         <h2 className="font-mono text-xl text-zinc-900 dark:text-white text-center line-clamp-2 mb-4">
           {order.description}
