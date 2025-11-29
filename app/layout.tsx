eb125f6f178fd6951547d53bf68bdf9169f6305e
Paulin <paulin.bonnefoy@outlook.fr>
2025-11-29 10:00:25 +0100

    feat: add pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import "@ant-design/v5-patch-for-react-19";
 
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
-import { ReactNode } from "react";
-import { Providers } from "@/lib/providers/Providers";
+import {ReactNode} from "react";
+import {Providers} from "@/lib/providers/Providers";
+import SWRegister from "@/app/sw-register";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Hotline Wei",
   description: "Author : Paulin & Malo Bonnefoy",
+    manifest: "/manifest.json",
 };
 
 export default async function RootLayout({
@@ -32,6 +34,7 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
+        <SWRegister />
       </body>
     </html>
   );
